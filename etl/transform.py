0b6fdfd22053ac8a3a368ed6ea381d4122d79049
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2025-10-14 18:15:42 +0530

    Update transform.py

--- etl/transform.py
+++ etl/transform.py
@@ -3,6 +3,10 @@ import numpy as np
 import janitor
 
 def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Perform data cleaning and transformation.
+    """
     df = df.clean_names()
+    df = df.dropna(how='all')
     df[df.select_dtypes(include=np.number).columns] = df.select_dtypes(include=np.number).fillna(0)
     return df
