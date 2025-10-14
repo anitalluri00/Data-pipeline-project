d7413dcb5cf07479f525ad6725a17fb865becf9e
Anirudh Talluri <168444331+anitalluri00@users.noreply.github.com>
2025-10-14 18:15:05 +0530

    Update extract.py

--- etl/extract.py
+++ etl/extract.py
@@ -3,10 +3,15 @@ import pandas as pd
 from bs4 import BeautifulSoup
 
 def extract_data():
-    # Example: Extract table data from Wikipedia
+    """
+    Extract data from a webpage and return a cleaned DataFrame.
+    Example uses Wikipedia GDP table.
+    """
     url = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)"
     response = requests.get(url)
+    response.raise_for_status()
     soup = BeautifulSoup(response.text, 'html.parser')
-    table = soup.find('table', {'class':'wikitable'})
-    df = pd.read_html(str(table))[0]
+    table = soup.find('table', {'class': 'wikitable'})
+    df_list = pd.read_html(str(table), flavor='lxml')
+    df = df_list[0]
     return df
